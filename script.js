79121c837d4b33ec264582e0080337b26bbd77ff
paozanwa22 <fauzanwadi7@gmail.com>
2020-10-18 08:57:44 +0800

    bermain Dengan Warna

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const sMerah = document.querySelector('input[name=sMerah]');    
+const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
+sMerah.addEventListener('input', function(){
+
+
+    const r = sMerah.value;     
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+
+
+});
+
+sHijau.addEventListener('input', function(){
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+sBiru.addEventListener('input', function(){
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
